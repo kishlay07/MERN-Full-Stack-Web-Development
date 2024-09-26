f207384c68bfe35b8ff3df41117134db0d3f02ad
kishlay07 <kishlaysinghrajput1999@gmail.com>
2024-09-27 02:03:26 +0530

    27th commit

--- JavaScript/12.Manipulating objects and array in Javascript.js
+++ JavaScript/12.Manipulating objects and array in Javascript.js
@@ -20,3 +20,19 @@ console.log(person['address']['Street']);
 
 console.log(myCars[1]);
 console.log(person.hobbies[0]);
+
+//2. Adding properties: You can add new properties to an object or elements to an array using the dot notation or the square bracket notation.
+
+person.skils = ["C","C++","Java","JS","SQL","HTML","CSS"];
+console.log(person);
+
+//3. Deleting properties: You can delete properties from an object using the delete keyword.
+
+delete person.age;
+console.log(person);
+
+//4. Looping through objects and arrays: 
+
+for(let key in person){
+    console.log(`${key} is ${person[key]}`);
+}
